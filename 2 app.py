06c80f1ffee073222b7190b1297e47eb530e7c59
Reneprogrammer <rene.mohoric@gmail.com>
2024-06-06 13:31:14 +0200

    Add files via upload

--- 2 app.py
+++ 2 app.py
@@ -1,54 +1,20 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a9e4dd14-c1ff-449f-9c0f-37f3b8699e1a",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import requests\n",
-    "from bs4 import BeautifulSoup\n",
-    "\n",
-    "def scrape_article(url):\n",
-    "    response = requests.get(url)\n",
-    "    if response.status_code == 200:\n",
-    "        soup = BeautifulSoup(response.content, 'html.parser')\n",
-    "        title = soup.find('h1').text if soup.find('h1') else 'No title found'\n",
-    "        content = ' '.join(p.text for p in soup.find_all('p'))\n",
-    "        return title, content\n",
-    "    else:\n",
-    "        return None, None\n",
-    "\n",
-    "url = 'https://www.economist.com/the-world-in-brief'\n",
-    "title, content = scrape_article(url)\n",
-    "if title and content:\n",
-    "    print(f\"Title: {title}\")\n",
-    "    print(f\"Content: {content[:500]}...\")  # Print the first 500 characters of the content\n",
-    "else:\n",
-    "    print(\"Failed to retrieve the article\")\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_article(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find('h1').text if soup.find('h1') else 'No title found'
+        content = ' '.join(p.text for p in soup.find_all('p'))
+        return title, content
+    else:
+        return None, None
+
+url = 'https://www.economist.com/the-world-in-brief'
+title, content = scrape_article(url)
+if title and content:
+    print(f"Title: {title}")
+    print(f"Content: {content[:500]}...")  # Print the first 500 characters of the content
+else:
+    print("Failed to retrieve the article")
